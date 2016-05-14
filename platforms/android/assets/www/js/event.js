8477cc10dbf26700ab1daa5de657e2bbc9974bd5
joonas <joonas.yoon@gmail.com>
2016-05-15 00:48:27 +0900

    날짜 선택 추가

--- platforms/android/assets/www/js/event.js
+++ platforms/android/assets/www/js/event.js
@@ -20,4 +20,34 @@ document.addEventListener("deviceready", function(){
 			location.href = href;
 		});
 	}
+
+	$('input[data-type="date-range"]').daterangepicker({
+		"singleDatePicker": true,
+		"timePicker": true,
+		"timePicker24Hour": true,
+		"timePickerIncrement": 5,
+		"autoApply": true,
+		"alwaysShowCalendars": true,
+		"startDate": "05/08/2016",
+		"endDate": "05/14/2016",
+		"opens": "center",
+		"locale": {
+			format: 'YYYY년 MM월 DD일 H시 mm분'
+		}
+	}, function(start, end, label) {
+		console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+	});
+
 }, false);
+
+function splitDate(text){
+	var r = text.split(/[^0-9\s]/);
+	return {
+		year: parseInt(r[0]),
+		mon : parseInt(r[1]),
+		day : parseInt(r[2]),
+		hour: parseInt(r[3]),
+		min : parseInt(r[4]),
+		sec : parseInt(r[5])
+	};
+}
